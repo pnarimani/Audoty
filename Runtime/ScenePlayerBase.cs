f6f8707aeb24b0fbd66b2c7ee2c5f255eb0456d7
Peyman Narimani <pmnarimani@gmail.com>
2021-05-16 17:32:52 +0430

    ScenePlayerBase will restore the name if audio clip is swapped.

--- Runtime/ScenePlayerBase.cs
+++ Runtime/ScenePlayerBase.cs
@@ -1,43 +1,45 @@
 using UnityEngine;
-
 #if ODIN_INSPECTOR
 using Sirenix.OdinInspector;
+
 #elif NAUGHTY_ATTRIBUTES
 using NaughtyAttributes;
 using ValueDropdown = NaughtyAttributes.DropdownAttribute;
+
 #endif
 
 namespace Audoty
 {
-    public abstract class ScenePlayerBase : MonoBehaviour
+    public abstract class ScenePlayerBase : MonoBehaviour, ISerializationCallbackReceiver
     {
         [SerializeField] private AudioPlayer _audio;
         [SerializeField] private bool _useRandomClip = true;
 
 #if UNITY_EDITOR && (ODIN_INSPECTOR || NAUGHTY_ATTRIBUTES)
-        [HideIf(nameof(UseRandomClip))]
-        [ValueDropdown(nameof(ClipNames))]
+        [HideIf(nameof(UseRandomClip)), ValueDropdown(nameof(ClipNames))] 
 #endif
+        // Don't include SerializeField in #if because it can cause some problems on IL2CPP
         [SerializeField]
-        private string _clipName;
+        private string _clipName = "";  
 
         [SerializeField, HideInInspector] private int _clipIndex;
-        
+
         protected AudioPlayer Audio => _audio;
 
         protected int ClipIndex => _clipIndex;
 
         protected bool UseRandomClip => _useRandomClip;
 
-        
+
 #if UNITY_EDITOR
-        private string[] ClipNames => _audio == null ? new string[0] : _audio.ClipNames;
-        
-        private void OnValidate()
+        private string[] ClipNames => _audio == null ? new string[] {null} : _audio.ClipNames;
+#endif
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize()
         {
             if (_audio == null)
             {
-                _clipName = null;
+                _clipName = "";
                 _clipIndex = -1;
                 return;
             }
@@ -47,6 +49,13 @@ namespace Audoty
             else
                 _clipIndex = Audio.FindIndex(_clipName);
         }
-#endif
+
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+            if (_audio == null)
+                return;
+
+            _clipName = Audio.Clips[_clipIndex].name;
+        }
     }
 }
